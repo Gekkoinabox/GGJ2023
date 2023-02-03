e8d3b033b3e2d7151fa21c73df6f377796380cd4
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2023-02-03 18:56:25 +0000

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] CharacterController player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
