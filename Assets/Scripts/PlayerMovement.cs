051d4a6c9eafb3b3d36b6501ba43a36d6ab2de95
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2023-02-04 13:37:40 +0000

    Character movement sussed kinda

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
@@ -7,9 +8,10 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] CharacterController player;
-    [SerializeField] Rigidbody2D rb;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpHeight = 5f;
+    [SerializeField] BoxCollider2D boxCollider;
+    bool isGrounded;
     //float velocity;
     float gravity = 1f;
     float yVelocity;
@@ -17,6 +19,11 @@ public class PlayerMovement : MonoBehaviour
     //public bool isGrounded = true;
     //float maxSpeed = 30f;
     //float acceleration = 2f;
+
+    void Start()
+    {
+        isGrounded = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -39,10 +46,32 @@ public class PlayerMovement : MonoBehaviour
         //    //Move Left
         //    player.Move(new Vector3(-speed, 0, 0) * Time.deltaTime);
         //}
+        
+        ////Store all collisions into an array
+        //Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
 
+        ////Check if on floor
+        //foreach(Collider2D hit in hits)
+        //{
+        //    if(hit == boxCollider)
+        //    {
+        //        continue;
+        //    }
+
+        //    ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+        //    if(colliderDistance.isOverlapped)
+        //    {
+        //        transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+        //    }
+        //}
+
+        //check if the player is touching the ground with a ray
+        //create ray
+        
 
         //Jump
-        if (player.isGrounded)
+        if (isGrounded)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -52,13 +81,31 @@ public class PlayerMovement : MonoBehaviour
             //player.Move(new Vector3(0, transform.position.y, 0) * Time.deltaTime);
             //isGrounded = false;
         } 
-
         else
         {
-            yVelocity -= gravity;
+            //yVelocity -= gravity;
         }
 
         velocity.y = yVelocity;
         player.Move(velocity * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1f);
+
+        if (hit.collider != null)
+        {
+            //check if we hit the ground
+            if (hit.collider.tag == "Ground")
+            {
+                isGrounded = true;
+                Debug.Log("ground has been hit");
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
     }
 }
