5159348c4149b3194214573397add8c7e2f17cef
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2023-02-04 16:26:04 +0000

    Added platforms to kitchen scene and starting on kill box logic

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            //Collided with player so kill them
+        }
+    }
+}
