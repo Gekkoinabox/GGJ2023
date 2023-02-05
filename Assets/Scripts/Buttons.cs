9fe7e0be9145e40967486c12a32ace4cf739c28a
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2023-02-05 14:23:24 +0000

    Stuff

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -9,4 +9,9 @@ public class Buttons : MonoBehaviour
     {
         SceneManager.LoadScene("Uprooting");
     }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartingScene");
+    }
 }
